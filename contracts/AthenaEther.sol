61544f1a144287f15889c5cd86302cc5ac42b071
Remix Workspace <>
2022-09-16 10:25:28 +0100

    Changed a little Math value.

--- contracts/AthenaEther.sol
+++ contracts/AthenaEther.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.14;
 
-import {ERC20} from "@openzeppelin/contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /**
 * @title AthenaEther.
@@ -13,11 +13,11 @@ contract AthenaEther is ERC20 {
     event AthenaLaunch(address, uint256);
 
     /// @dev Constructor, mints 1 billion tokens to the Wrap contract.
-    /// @param _AETH Address of AthenaWrap contract.
+    /// @param _wrap Address of AthenaWrap contract.
     constructor(address _wrap) ERC20("AthenaEther", "AETH") {
         /// @dev Mint 1 billion tokens to the AthenaEther Contract.
-        _mint(_wrap, (1e9) ** 18);
+        _mint(_wrap, (10 ** 9) * (10 ** 18));
         /// @dev Emit {AthenaLaunch} event.
-        emit AthenaLaunch(_wrap, (1e9) ** 18);
+        emit AthenaLaunch(_wrap, (10 ** 9) * (10 ** 18));
     }
 }
