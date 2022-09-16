3f136e54072b4ce549743f6666cda66b26302699
Remix Workspace <>
2022-09-16 14:36:32 +0100

    Added interface and contracts.

--- contracts/AthenaEther.sol
+++ contracts/AthenaEther.sol
@@ -6,7 +6,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 /**
 * @title AthenaEther.
 * @author Anthony (fps) https://github.com/0xfps.
-* @dev 
+* @dev Contract for minted AthenaEther to be transferred on successful wraps.
 */
 contract AthenaEther is ERC20 {
     /// @dev Emitted on a new Deployment.
@@ -16,8 +16,8 @@ contract AthenaEther is ERC20 {
     /// @param _wrap Address of AthenaWrap contract.
     constructor(address _wrap) ERC20("AthenaEther", "AETH") {
         /// @dev Mint 1 billion tokens to the AthenaEther Contract.
-        _mint(_wrap, (10 ** 9) * (10 ** 18));
+        _mint(_wrap, (1 ** 9) * (10 ** 18));
         /// @dev Emit {AthenaLaunch} event.
-        emit AthenaLaunch(_wrap, (10 ** 9) * (10 ** 18));
+        emit AthenaLaunch(_wrap, (1 ** 9) * (10 ** 18));
     }
 }
